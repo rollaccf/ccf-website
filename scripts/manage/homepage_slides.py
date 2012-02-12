aac3aa786a4bd78d0c2bdb17559d42d298c228d9
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-02-12 16:11:41 -0600

    Update newSlide page and POST handler

--- scripts/manage/homepage_slides.py
+++ scripts/manage/homepage_slides.py
@@ -1,3 +1,4 @@
+import os
 from os.path import join
 from google.appengine.api import images, memcache
 from google.appengine.ext import webapp
@@ -19,16 +20,22 @@ class ManageHomePageSlidesHandler(BaseHandler):
 
 class ManageNewSlideHandler(BaseHandler):
     def get(self):
-      self.render_template(join("manage", "homepage_slides", "new_slide.html"), { 'title':"New Homepage Slide", 'MaxHomepageSlides':gaesettings.MaxHomepageSlides})
+      self.render_template(join("manage", "homepage_slides", "new_slide.html"),
+        { 'title':"New Homepage Slide",
+          'MaxHomepageSlides':gaesettings.MaxHomepageSlides,
+          'LinkPrefix':'/'.join((os.environ['HTTP_HOST'], gaesettings.HomepageLinkPrefix)),
+      })
     def post(self):
       # TODO: add cgi escape
-      # TODO: add html handling stuff
       # TODO: add error checking
       enabled = self.request.get("enabled")
       slideImage = images.resize(self.request.get("image"), 400, 300)
       link = self.request.get("link")
+      html = self.request.get("slideHtml")
 
-      newSlide = HomepageSlide(Enabled=bool(enabled), Link=link, Image=slideImage)
+      # BUG: This will fail on https hosts
+      link = '/'.join(("http:/", os.environ['HTTP_HOST'], gaesettings.HomepageLinkPrefix, link))
+      newSlide = HomepageSlide(Enabled=bool(enabled), Link=link, Image=slideImage, Html=html)
       newSlide.put()
 
       if enabled:
