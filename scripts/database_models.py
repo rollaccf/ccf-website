fad7409e0a67c52da4adfe2ab2d2b9da9754d814
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-02-18 23:24:14 -0600

    Add 'required' parameters to HomepageSlide

--- scripts/database_models.py
+++ scripts/database_models.py
@@ -1,16 +1,15 @@
 from google.appengine.ext import db
 
-# TODO: add required=True to necessary fields
 class HomepageSlide(db.Model):
-  Enabled = db.BooleanProperty()
+  Enabled = db.BooleanProperty(required=True)
 
   Createdby = db.UserProperty(auto_current_user_add=True)
   CreationDateTime = db.DateTimeProperty(auto_now_add=True)
   Modifiedby = db.UserProperty(auto_current_user=True)
   ModifiedDateTime = db.DateTimeProperty(auto_now=True)
 
-  Image = db.BlobProperty()
-  Link = db.LinkProperty()
-  Title = db.StringProperty()
-  Html = db.TextProperty()
+  Image = db.BlobProperty(required=True)
+  Link = db.LinkProperty(required=True)
+  Title = db.StringProperty(required=True)
+  Html = db.TextProperty(required=True)
   #TODO: add order property
