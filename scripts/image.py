b0cd8dbe5f5bfd9a05634a04eb59432f522c0676
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-07-16 22:56:36 -0400

    Clean up image.py

--- scripts/image.py
+++ scripts/image.py
@@ -1,19 +1,20 @@
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from scripts.database_models import HomepageSlide
-from scripts.main import BaseHandler
+from scripts.main import BaseHandler, Http404
 
-class Image(webapp.RequestHandler):
-  def get(self):
-    key = self.request.get("key")
-    if key:
-      slide = db.get(key)
+class Image(BaseHandler):
+  def get(self, encoded_key):
+    try:
+      slide = db.get(encoded_key)
       if slide.Image:
         self.response.headers['Content-Type'] = "image/png"
         self.response.out.write(slide.Image)
-      else:
-        self.response.out.write("No image")
+    except:
+      raise Http404("Image Does Not Exist")
 
 
-application = webapp.WSGIApplication([('/image', Image)], debug=BaseHandler.debug)
+application = webapp.WSGIApplication([
+  ('/image/([^/]+)', Image),
+  ], debug=BaseHandler.debug)
 
