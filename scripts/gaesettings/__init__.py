b470a887a727eaad15dcd497f9f30b6d241d1587
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-07-22 22:45:29 -0400

    Update gaesettings to use ndb

--- scripts/gaesettings/__init__.py
+++ scripts/gaesettings/__init__.py
@@ -1,7 +1,6 @@
 """Simple configurable settings module for GAE"""
-from google.appengine.api import memcache
-from google.appengine.ext import db
-from google.appengine.ext.db import polymodel, GqlQuery
+from google.appengine.ext import ndb
+from google.appengine.ext.ndb import polymodel
 
 ##  Name:(defaultValue, Catagory, DisplayName, Documentation, ReadOnlyBool)
 DefaultValues = {
@@ -32,20 +31,20 @@ DefaultValues = {
 }
 
 class BaseSetting(polymodel.PolyModel):
-  ReadOnly = db.BooleanProperty()
-  Name = db.StringProperty()
-  Catagory = db.CategoryProperty()
-  DisplayName = db.StringProperty() # I would use verbose_name instead, but I can't figure out how to use it
-  Documentation = db.StringProperty(multiline=True)
+  ReadOnly = ndb.BooleanProperty()
+  Name = ndb.StringProperty()
+  Catagory = ndb.StringProperty()
+  DisplayName = ndb.StringProperty() # I would use verbose_name instead, but I can't figure out how to use it
+  Documentation = ndb.StringProperty()
 
 class StringSetting(BaseSetting):
-  Value = db.StringProperty()
+  Value = ndb.StringProperty()
 
 class IntSetting(BaseSetting):
-  Value = db.IntegerProperty()
+  Value = ndb.IntegerProperty()
 
 class FloatSetting(BaseSetting):
-  Value = db.FloatProperty()
+  Value = ndb.FloatProperty()
 
 class GAESettingDoesNotExist(Exception):
   pass
@@ -58,37 +57,25 @@ class GAESettingReadOnlyError(Exception):
 
 class _gaesettings(object):
   def __getattr__(self, name):
-    value = memcache.get("gaesettings_"+name)
-    if value == None:
-      dbValue = GqlQuery("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
-      if dbValue == None:
-        self.CreateNonexistantValuesInDataStore()
-        dbValue = GqlQuery("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
-        if dbValue == None:
-          raise GAESettingDoesNotExist("'" +name + "' does not exist in the default values or in the datastore")
-      memcache.set("gaesettings_"+name, dbValue.Value)
-      return dbValue.Value
-    else:
-      return value
-
-  def __setattr__(self, name, value):
-    dbValue = GqlQuery("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
+    dbValue = ndb.gql("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
     if dbValue == None:
       self.CreateNonexistantValuesInDataStore()
-      dbValue = GqlQuery("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
+      dbValue = ndb.gql("SELECT * FROM BaseSetting WHERE Name = :1", name).get();
       if dbValue == None:
-        raise GAESettingDoesNotExist("'" +name + "' does not exist in the datastore")
+        raise GAESettingDoesNotExist("'" +name + "' does not exist in the default values or in the datastore")
+    return dbValue.Value
+
+  def __setattr__(self, name, value):
+    dbValue = self.__getattr__(name)
     if dbValue.ReadOnly == True:
       raise GAESettingReadOnlyError("Can not set the value of '" +name + "' it is read only")
     dbValue.Value = value
     dbValue.put()
-    memcache.set("gaesettings_"+name, dbValue.Value)
 
   def CreateNonexistantValuesInDataStore(self):
-    q = GqlQuery("SELECT * FROM BaseSetting WHERE Name = :1")
+    unbound_query = ndb.gql("SELECT * FROM BaseSetting WHERE Name = :1")
     for key,value in DefaultValues.items():
-      q.bind(key)
-      dbValue = q.get()
+      dbValue = unbound_query.bind(key).get()
       if dbValue == None:
         if isinstance(value[0], basestring):
           StringSetting(Name=key, Value=value[0], Catagory=value[1], DisplayName=value[2], Documentation=value[3], ReadOnly=value[4]).put()
