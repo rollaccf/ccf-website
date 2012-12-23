ca97ac1595195e3dca9f6eb30d55ad863deb30c4
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-12-22 20:41:53 -0600

    Add /manage/setup

--- scripts/manage/__init__.py
+++ scripts/manage/__init__.py
@@ -5,7 +5,25 @@ class ManageHandler(BaseHandler):
     def get(self):
         self.render_template("manage/manage.html")
 
+class SetupHandler(BaseHandler):
+    def get(self):
+        # https://developers.google.com/appengine/articles/update_schema?hl=en
+
+        # Swap the base class of <model> to db.Expando
+        # Run this script
+        # Swap the base clss back to db.model
+
+        # 22 December 2012
+        # update HousingApplication.SemesterToBegin to HousingApplication.SemesterToBeginIndex
+	from scripts.database_models.housingapplication import HousingApplication, get_index_from_semester_text
+        q = HousingApplication.all()
+        for app in q:
+            self.response.write("{}<br />".format(app.SemesterToBegin))
+            app.SemesterToBeginIndex = get_index_from_semester_text(app.SemesterToBegin)
+          # del app.SemesterToBegin
+
 
 application = webapp.WSGIApplication([
+  ('/manage/setup', SetupHandler),
   ('/manage.*', ManageHandler),
   ], debug=BaseHandler.debug)
