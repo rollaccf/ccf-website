010297cfeb28484db4ee42633921cceddddd238f
Jordan Wright <Wright.Jordan.M@gmail.com>
2012-12-27 10:51:01 -0600

    Add appstats

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,11 @@
-from scripts.gaesessions import SessionMiddleware
 
 def webapp_add_wsgi_middleware(app):
-  return SessionMiddleware(app, cookie_key="fm19dewrg49san}[HAN73(lrwimQ23lnhfgking")
+    # appstats https://developers.google.com/appengine/docs/python/tools/appstats
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+
+    # gaesessions https://github.com/dound/gae-sessions
+    from scripts.gaesessions import SessionMiddleware
+    app = SessionMiddleware(app, cookie_key="fm19dewrg49san}[HAN73(lrwimQ23lnhfgking")
+    return app
 
