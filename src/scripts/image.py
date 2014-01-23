3dbfbeaff47ff1a781980a610a2d09bb0751b6ed
Jordan Wright <Wright.Jordan.M@gmail.com>
2014-01-22 21:43:06 -0500

    add head function to image.py

--- src/scripts/image.py
+++ src/scripts/image.py
@@ -10,6 +10,15 @@ from database_models.student_officer import StudentOfficer
 
 
 class Image(BaseHandler):
+    def head(self):
+        two_days_in_seconds = 172800
+        expires_date = datetime.datetime.now() + datetime.timedelta(days=2)
+        HTTP_HEADER_FORMAT = "%a, %d %b %Y %H:%M:00 GMT"
+
+        self.response.headers['Expires'] = expires_date.strftime(HTTP_HEADER_FORMAT)
+        self.response.headers['Cache-Control'] = "public, max-age=%s" % two_days_in_seconds
+        self.response.headers['Content-Type'] = "image/png"
+
     def get(self, encoded_key):
         model = None
         try:
