e0687165a502b5803dddd43687ac2a3880bc18bf
Jordan Wright <Wright.Jordan.M@gmail.com>
2014-09-05 23:38:28 -0400

    Disable app stats

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,8 +1,8 @@
 
 def webapp_add_wsgi_middleware(app):
     # appstats https://developers.google.com/appengine/docs/python/tools/appstats
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
+    #from google.appengine.ext.appstats import recording
+    #app = recording.appstats_wsgi_middleware(app)
 
     # gaesessions https://github.com/dound/gae-sessions
     from ext.gaesessions import SessionMiddleware
